ac434e1a4b52e56f6eb1aee975ff8702fe84ad68
CHERRAD Charef Eddine <ccherrad@gmail.com>
2018-12-28 19:46:30 +0100

    adding models

--- appointment_manager/main_application/models.py
+++ appointment_manager/main_application/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class user(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name  = models.CharField(max_length=30)
+	email = models.EmailField(max_length=70)
+	phone_number = models.CharField(max_length = 20)
+	password = models.CharField(max_length=70)
+
+class office(models.Model):
+	office_name = models.CharField(max_length = 70)
+	office_profession = models.CharField(max_length = 50)
+	wialaya = models.CharField(max_length = 20) 
+	address = models.CharField(max_length = 70)  
+	user = models.ForeignKey(user,on_delete=models.CASCADE) 
+
+class contact(models.Model): 
+	office_phone_number = models.CharField(max_length = 20)
+	office_email = models.EmailField(max_length = 70)
+	office = models.ForeignKey(office, on_delete = models.CASCADE)
+	
+
+
+class appointmenet(models.Model):
+	user = models.ForeignKey(user,on_delete=models.CASCADE)
+	office = models.ForeignKey(office,on_delete=models.CASCADE)
+	timestamp = models.DateTimeField()
+	state = models.CharField(max_length = 10)
+
+
+class rating(models.Model):
+	user = models.ForeignKey(user,on_delete=models.CASCADE)
+	office = models.ForeignKey(office,on_delete=models.CASCADE)
+	rating = models.IntegerField()
+	review = models.CharField(max_length = 100)
