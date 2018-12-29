fcf27a1a9c17542d33c98fc5cce703b16e1ae9e2
CHERRAD Charef Eddine <ccherrad@gmail.com>
2018-12-29 01:49:13 +0100

    adding models to admin

--- appointment_manager/main_application/admin.py
+++ appointment_manager/main_application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import * 
+
+admin.site.register(user)
+admin.site.register(office)
+admin.site.register(address)
+admin.site.register(contact)
+admin.site.register(appointmenet)
+admin.site.register(rating)
+
+
+
 # Register your models here.
